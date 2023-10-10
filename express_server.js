7807d81197b1a2977f82b44a14936f19c8d89eb0
wingkoon <wingkoon@yahoo.com>
2023-10-09 18:04:53 -0700

    partial header

---
+++ express_server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs");
+
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/urls.json", (req, res) => {
+    res.json(urlDatabase);
+  });
+
+app.get("/hello", (req, res) => {
+    res.send("<html><body>Hello <b>World</b></body></html>\n");
+  });
+
+  app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
+
+  app.get("/hello", (req, res) => {
+    const templateVars = { greeting: "Hello World!" };
+    res.render("hello_world", templateVars);
+  });
+
+  app.get("/urls/:id", (req, res) => {
+    const templateVars = { id: req.params.id, longURL: /* What goes here? */ };
+    res.render("urls_show", templateVars);
+  });
+
+  curl -i http://localhost:8080/hello

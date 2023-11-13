28300ebad96e9fb322459ea429ee25d020791be3
wingkoon <wingkoon@yahoo.com>
2023-11-13 10:15:09 -0800

    fix the edit function

--- express_server.js
+++ express_server.js
@@ -4,7 +4,10 @@ const PORT = 8080; // default port 8080
 const bcrypt = require("bcryptjs");
 const password = "purple-monkey-dinosaur"; // found in the req.body object
 const hashedPassword = bcrypt.hashSync(password, 10);
+const cookieParser = require('cookie-parser');
 
+// Set cookie-parser as a middleware
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 
@@ -17,6 +20,26 @@ const generateRandomString = function() {
   const result = Math.random().toString(36).substring(2,8);
   return result;
 };
+
+// Sign-in endpoint
+app.post('/sign-in', (req, res) => {
+  console.log("hi");
+  // Set the cookie with whatever value you wish
+  res.cookie('user_id', userObject.id);
+
+  // Finish the request-response cycle with 
+  // res.redirect(), res.send() or res.json():
+  res.redirect('/');
+})
+
+// Home endpoint
+app.get('/', (req, res) => {
+  // We can read cookies with req.cookies.cookieName
+  const idUser = req.cookies.user_id;
+
+  if (!idUser) return res.send('No cookie stored...');
+  return res.send('There is a cookie stored!');
+});
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -47,7 +70,7 @@ app.get("/urls.json", (req, res) => {
   app.get("/urls/:id", (req, res) => {
     let longURL = urlDatabase[req.params.id];
     const templateVars = { id: req.params.id, longURL: longURL };
-    res.render("urls/:id", templateVars);
+    res.render(`urls_show.ejs`, templateVars);
   }); 
   
   app.get("/hello", (req, res) => {
@@ -55,17 +78,30 @@ app.get("/urls.json", (req, res) => {
     res.render(`hello_world`, templateVars);
   });
 
-  app.get("/urls/:id", (req, res) => {
-    let longURL = urlDatabase[req.params.id];
-    const templateVars = { id: req.params.id, longURL: longURL };
-    res.render(`urls_show`, templateVars);
+  app.get("/urls/:id/edit", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render(`urls_index.ejs`, templateVars);
   });
 
-  app.post("/urls/:id", (req, res) => {
-    let longURL = urlDatabase[req.params.id];
-    const templateVars = { id: req.params.id, longURL: longURL };
-    res.render(`urls_show`, templateVars);
+  app.post("/urls/edit/:id", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+   res.redirect(`/urls`);
   });
+
+  app.post("/urls/:id/edit", (req, res) => {
+   let longURL = req.body.longURL;
+    if (longURL === "") {
+      res.send("Null input");
+      res.redirect(`urls/:id`);
+    } else {
+      let id = req.params.id;
+      urlDatabase[id] = longURL;
+      res.send("longURL edited");
+      res.redirect('/urls');
+    } 
+  });
+  
+
 
   //POST /urls/:id/delete
   app.post("/urls/:id/delete", (req, res) => {
